969465ceaa37a6d7322a6b5553704c6e5446dfc2
Dmitry Murmachyov <murmachyov@gmail.com>
2022-02-16 13:48:48 +0300

    Initial Commit

---
+++ trafаicLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trafаicLight
+//
+//  Created by Murmachyov on 16.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
